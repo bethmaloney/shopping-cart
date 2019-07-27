f1998d3e05de0abc1ddb67a2057201619675ee1e
Bethany Maloney <beth@Bethanys-MacBook-Pro.local>
2019-07-27 10:57:42 +1000

    add css modules

--- src/components/header/cart-preview/cart-preview-view.tsx
+++ src/components/header/cart-preview/cart-preview-view.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { IProps } from './types/types-view';
 import PrimaryButton from '../../buttons/PrimaryButton';
+import styles from "./cart-preview.module.scss";
 
-const View: React.FC<IProps> = ({products}) => {
+const View: React.FC<IProps> = ({ products }) => {
   const emptyCart = (
     <div>
       <PrimaryButton>Continue Shopping</PrimaryButton>
@@ -11,19 +12,27 @@ const View: React.FC<IProps> = ({products}) => {
 
   const cartWithItems = (
     <div>
-    {products.map(product => {
-      return (
-        <div>
-          {product.name}
-        </div>
-      )
-    })}
-    <PrimaryButton>Checkout Securely</PrimaryButton>
-  </div>
+      {products.map(product => {
+        return (
+          <div>
+            <div className={styles.container}>
+              <img src={product.imageUrl} alt="Product Preview" width="100"/>
+            </div>
+            <div className={styles.descriptionContainer}>
+            {product.name}
+            </div>
+            <div className={styles.priceContainer}>
+            </div>
+            Price
+          </div>
+        )
+      })}
+      <PrimaryButton>Checkout Securely</PrimaryButton>
+    </div>
   );
 
   return (
-    <div className="shadow p-3 mb-5 bg-white rounded">
+    <div className="cart-preview-container">
       {products.length > 0 ? cartWithItems : emptyCart}
     </div>
   )
