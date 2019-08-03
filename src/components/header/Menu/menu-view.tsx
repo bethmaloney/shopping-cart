b45ae8fe6a15e33a186d867bfce60494a4857460
Bethany Maloney <bethany.d.maloney@gmail.com>
2019-08-03 10:09:14 +1000

    fix menu on mobile

--- src/components/header/Menu/menu-view.tsx
+++ src/components/header/Menu/menu-view.tsx
@@ -7,10 +7,11 @@ import styles from "./menu.module.scss";
 const View: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Fälschung</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
+      <a className="navbar-brand" href="#">Fälschung</a>
+
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
@@ -28,10 +29,11 @@ const View: React.FC = () => {
           <input className="form-control mr-sm-2" type="search" placeholder="Search Items" aria-label="Search" />
           <PrimaryButton>Search</PrimaryButton>
         </form>
-        <div className={styles.shoppingBag}>
-          <FontAwesomeIcon icon={faShoppingBag} size="2x"/>
-        </div>
+
       </div>
+      <button className={styles.shoppingBag}>
+          <FontAwesomeIcon icon={faShoppingBag} size="2x"/>
+        </button>
     </nav>
   )
 }
