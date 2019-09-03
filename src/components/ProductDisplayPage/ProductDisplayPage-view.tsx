6c281df560417d341569b5aab6facd8465a10be2
Bethany Maloney <bethany.d.maloney@gmail.com>
2019-09-04 08:35:13 +1000

    improve it a bit more

--- src/components/ProductDisplayPage/ProductDisplayPage-view.tsx
+++ src/components/ProductDisplayPage/ProductDisplayPage-view.tsx
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import { ProductInstance } from "./types/ProductInstances";
+import React from "react";
 import { Product } from "./types/Product";
 import styles from "./ProductDisplayPage.module.scss";
+import { formatToCurrency } from "../../utility";
 
 
 export type Props = {
   product: Product;
-  productInstances: ProductInstance[];
+  colours: string[];
+  sizes: string[];
+  price: number;
 }
 
 const imageLink = (text: string) => `https://via.placeholder.com/500x1200.png?text=${text}`;
 
-const View: React.FC<Props> = ({product: products, productInstances}) => {
-  const [currentProductInstance, updateProductInstance] = useState(productInstances[0])
-
+const View: React.FC<Props> = ({ product, colours, sizes, price }) => {
   return (
     <div>
       <div className="row">
         <div className={styles.productImage}>
-          <img src={imageLink(products.name)}/>
+          <img src={imageLink(product.name)} alt={product.description} />
         </div>
         <div className="col sm-6">
-          <p>{products.name}</p>
-          <p>{products.description}</p>
-          <p>{currentProductInstance.price}</p>
+          <p className={styles.productName}>{product.name}</p>
+          <p className={styles.productDescription}>{product.description}</p>
+          <p className={styles.price}>{formatToCurrency(price)}</p>
 
           <select className="form-control">
-            {productInstances.map(instance => <option>{instance.colour}</option>)}
+            {colours.map(colour => <option>{colour}</option>)}
           </select>
           <select className="form-control">
-            {productInstances.map(instance => <option>{instance.size}</option>)}
+            {sizes.map(size => <option>{size}</option>)}
           </select>
         </div>
       </div>
--- src/components/ProductDisplayPage/types/ProductInstances.ts
+++ src/components/ProductDisplayPage/types/ProductInstances.ts
@@ -1,4 +1,5 @@
 export interface ProductInstance {
+  id: string;
   price: number;
   colour: string;
   size: string;
