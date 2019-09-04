20905d17b93a512bb19bd216f6df991a20d2921c
Bethany Maloney <bethany.d.maloney@gmail.com>
2019-09-04 18:19:08 +1000

    improve formatting

--- src/components/ProductDisplayPage/ProductDisplayPage-view.tsx
+++ src/components/ProductDisplayPage/ProductDisplayPage-view.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Product } from "./types/Product";
 import styles from "./ProductDisplayPage.module.scss";
 import { formatToCurrency } from "../../utility";
+import PrimaryButton from "../buttons/PrimaryButton";
 
 
 export type Props = {
@@ -11,26 +12,37 @@ export type Props = {
   price: number;
 }
 
-const imageLink = (text: string) => `https://via.placeholder.com/500x1200.png?text=${text}`;
+const imageLink = (text: string) => `https://via.placeholder.com/900x1200.png?text=${text}`;
 
 const View: React.FC<Props> = ({ product, colours, sizes, price }) => {
   return (
-    <div>
+    <div className={styles.displayPageContainer}>
       <div className="row">
         <div className={styles.productImage}>
           <img src={imageLink(product.name)} alt={product.description} />
         </div>
-        <div className="col sm-6">
+        <div className={styles.productInfo}>
           <p className={styles.productName}>{product.name}</p>
           <p className={styles.productDescription}>{product.description}</p>
           <p className={styles.price}>{formatToCurrency(price)}</p>
 
-          <select className="form-control">
-            {colours.map(colour => <option>{colour}</option>)}
-          </select>
-          <select className="form-control">
-            {sizes.map(size => <option>{size}</option>)}
-          </select>
+          <form>
+            <div className="form-group">
+              <select className="form-control">
+                {colours.map(colour => <option>{colour}</option>)}
+              </select>
+            </div>
+
+            <div className="form-group">
+              <select className="form-control">
+                {sizes.map(size => <option>{size}</option>)}
+              </select>
+            </div>
+
+            <div className="form-group">
+              <PrimaryButton className={styles.addToBagButton}>Add to bag</PrimaryButton>
+            </div>
+          </form>
         </div>
       </div>
     </div>
